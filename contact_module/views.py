f564dfcf029e953ff5b4bf3d047e7d3b1314dfa9
unknown <shahirgulzar535@gmail.com>
2023-09-06 22:49:36 -0800

    till video 72

--- contact_module/views.py
+++ contact_module/views.py
@@ -1,14 +1,50 @@
 from django.shortcuts import render, redirect
+from django.views import View
+
+from .forms import ContactUsForm, ContactUsModelForm
+from .models import ContactUs
 
 # Create your views here.
 from django.urls import reverse
 
 
+class ContactUsView(View):
+    def get(self, request):
+        contact_form = ContactUsModelForm()
+        return render(request, 'contact_module/contact_us_page.html', {
+            'contact_form': contact_form
+        })
+
+    def post(self, request):
+        contact_form = ContactUsModelForm(request.POST)
+        if contact_form.is_valid():
+            contact_form.save()
+            return redirect('home_page')
+
+        return render(request, 'contact_module/contact_us_page.html', {
+            'contact_form': contact_form
+        })
+
+
 def contact_us_page(request):
     if request.method == 'POST':
-        print(request.POST['email'])
-        print(request.POST['fullname'])
-        print(request.POST['subject'])
-        print(request.POST['message'])
-        return redirect(reverse('home_page'))
-    return render(request, 'contact_module/contact_us_page.html', {})
+        # contact_form = ContactUsForm(request.POST)
+        contact_form = ContactUsModelForm(request.POST)
+        if contact_form.is_valid():
+            # print(contact_form.cleaned_data)
+            # contact = ContactUs(
+            #     title=contact_form.cleaned_data.get('title'),
+            #     full_name=contact_form.cleaned_data.get('full_name'),
+            #     email=contact_form.cleaned_data.get('email'),
+            #     message=contact_form.cleaned_data.get('message'),
+            # )
+            # contact.save()
+            contact_form.save()
+            return redirect('home_page')
+    else:
+        # contact_form = ContactUsForm()
+        contact_form = ContactUsModelForm()
+
+    return render(request, 'contact_module/contact_us_page.html', {
+        'contact_form': contact_form
+    })
--- eshop_project/settings.py
+++ eshop_project/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'product_module',
     'home_module',
     'contact_module',
-    'account_module',
     'django_render_partial',
 ]
 
@@ -124,9 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
-MEDIA_ROOT = BASE_DIR / 'uploads'
-MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
