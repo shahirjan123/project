c358671e856d948db87d724e9a211bbff93e1052
unknown <shahirgulzar535@gmail.com>
2023-09-17 12:13:09 -0800

    dynamic most features #111

--- product_module/urls.py
+++ product_module/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.product_list, name='product-list'),
     path('', views.ProductListView.as_view(), name='product-list'),
-    # path('<slug:slug>', views.product_detail, name='product-detail'),
+    path('cat/<cat>', views.ProductListView.as_view(), name='product-categories-list'),
+    path('brand/<brand>', views.ProductListView.as_view(), name='product-list-by-brands'),
     path('product-favorite', views.AddProductFavorite.as_view(), name='product-favorite'),
     path('<slug:slug>', views.ProductDetailView.as_view(), name='product-detail'),
 ]
--- product_module/views.py
+++ product_module/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from django.views.generic.base import TemplateView, View
+from django.db.models import Count
+from django.http import HttpRequest
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
-from .models import Product
+from django.views.generic.base import View
+from .models import Product, ProductCategory, ProductBrand
 
 
 class ProductListView(ListView):
@@ -9,11 +11,37 @@ class ProductListView(ListView):
     model = Product
     context_object_name = 'products'
     ordering = ['-price']
-    paginate_by = 1
-    # def get_queryset(self):
-    #     base_query = super(ProductListView, self).get_queryset()
-    #     data = base_query.filter(is_active=True)
-    #     return data
+    paginate_by = 6
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(ProductListView, self).get_context_data()
+        query = Product.objects.all()
+        product: Product = query.order_by('-price').first()
+        db_max_price = product.price if product is not None else 0
+        context['db_max_price'] = db_max_price
+        context['start_price'] = self.request.GET.get('start_price') or 0
+        context['end_price'] = self.request.GET.get('end_price') or db_max_price
+        return context
+
+    def get_queryset(self):
+        query = super(ProductListView, self).get_queryset()
+        category_name = self.kwargs.get('cat')
+        brand_name = self.kwargs.get('brand')
+        request: HttpRequest = self.request
+        start_price = request.GET.get('start_price')
+        end_price = request.GET.get('end_price')
+        if start_price is not None:
+            query = query.filter(price__gte=start_price)
+
+        if end_price is not None:
+            query = query.filter(price__lte=end_price)
+
+        if brand_name is not None:
+            query = query.filter(brand__url_title__iexact=brand_name)
+
+        if category_name is not None:
+            query = query.filter(category__url_title__iexact=category_name)
+        return query
 
 
 class ProductDetailView(DetailView):
@@ -35,3 +63,19 @@ class AddProductFavorite(View):
         product = Product.objects.get(pk=product_id)
         request.session["product_favorites"] = product_id
         return redirect(product.get_absolute_url())
+
+
+def product_categories_component(request: HttpRequest):
+    product_categories = ProductCategory.objects.filter(is_active=True, is_delete=False)
+    context = {
+        'categories': product_categories
+    }
+    return render(request, 'product_module/components/product_categories_component.html', context)
+
+
+def product_brands_component(request: HttpRequest):
+    product_brands = ProductBrand.objects.annotate(products_count=Count('product')).filter(is_active=True)
+    context = {
+        'brands': product_brands
+    }
+    return render(request, 'product_module/components/product_brands_component.html', context)
